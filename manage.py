d1ef7dda0eb77df162004de49e31bcc8c6b6f517
Roni Choudhury <roni.choudhury@kitware.com>
2025-03-07 11:30:18 -0500

    Rename the project to "hips"

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hipsdb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hips.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
