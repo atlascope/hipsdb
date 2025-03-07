4ee141f12f09f61202fd5fa16e406093b1040e34
Roni Choudhury <roni.choudhury@kitware.com>
2025-03-07 10:44:10 -0500

    Add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Nix dependencies for running the Heroku tutorial";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python310Packages.python
+        ];
+      };
+  };
+}
