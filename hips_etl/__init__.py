7cb83f8d49f3aec709928400a8604853e8221478
Roni Choudhury <roni.choudhury@kitware.com>
2025-03-14 10:27:34 -0400

    Add structural checking and one data integrity check
    This will need to be refactored for readability and to eliminate code
    duplication.

--- hips_etl/__init__.py
+++ hips_etl/__init__.py
@@ -1,3 +1,6 @@
+import csv
+import importlib.resources
+import json
 from pathlib import Path
 import re
 
@@ -32,11 +35,22 @@ def validate_hips_data_dir(data_dir: Path):
     if {x.name for x in meta_files} != {x.name for x in props_files}:
         raise ValueError(f"Files in {meta_dir} and {props_dir} do not match.")
 
+    with open(importlib.resources.files(__package__) / "fields" / "common.json") as f:
+        common_fields = set(json.load(f))
+
+    with open(importlib.resources.files(__package__) / "fields" / "meta_only.json") as f:
+        meta_only_fields = set(json.load(f))
+
+    with open(importlib.resources.files(__package__) / "fields" / "props_only.json") as f:
+        props_only_fields = set(json.load(f))
+
     # Capture one set of just the common file basenames.
     filenames = {x.name for x in meta_files}
 
     # Check that each filename matches the correct pattern.
     for filename in filenames:
+        print(f'Validating {filename}...')
+
         match = csv_filename_pattern.match(filename)
         if not match:
             raise ValueError(f"Filename {filename} does not match the pattern.")
@@ -44,3 +58,59 @@ def validate_hips_data_dir(data_dir: Path):
         # Check that the case name matches the directory name.
         if match.group('case_name') != data_dir.name:
             raise ValueError(f"Case name for {filename} does not match directory name {data_dir.name}.")
+
+        with open(meta_dir / filename) as f:
+            reader = csv.DictReader(f)
+            meta_fields = set(reader.fieldnames)
+            meta_rows = list(reader)
+
+        if not all(k in meta_fields for k in common_fields):
+            raise ValueError(f"Meta fields missing some common keys.")
+
+        if not all(k in meta_fields for k in meta_only_fields):
+            raise ValueError(f"Meta fields missing some meta-only keys.")
+
+        if len(meta_fields) > len(common_fields) + len(meta_only_fields):
+            raise ValueError(f"Unexpected meta fields.")
+
+        with open(props_dir / filename) as f:
+            reader = csv.DictReader(f)
+            props_fields = set(reader.fieldnames)
+            props_rows = list(reader)
+
+        if not all(k in props_fields for k in common_fields):
+            raise ValueError(f"Props fields missing some common keys.")
+
+        if not all(k in props_fields for k in props_only_fields):
+            raise ValueError(f"Props fields missing some props-only keys.")
+
+        if len(props_fields) > len(common_fields) + len(props_only_fields):
+            raise ValueError(f"Unexpected props fields.")
+
+        # Check that meta and props contain the same number of rows and matching row IDs.
+        if len(meta_rows) != len(props_rows):
+            raise ValueError(f"Meta and props files have different number of rows for {filename}.")
+
+        if set(int(float(row['Identifier.ObjectCode'])) for row in meta_rows) != set(int(float(row['Identifier.ObjectCode'])) for row in props_rows):
+            raise ValueError(f"Meta and props files have different ObjectCodes for {filename}.")
+
+        # Construct a mapping from id to row for both meta and props.
+        meta_dict = {int(float(row['Identifier.ObjectCode'])): row for row in meta_rows}
+        props_dict = {int(float(row['Identifier.ObjectCode'])): row for row in props_rows}
+
+        # Check the data integrity properties between meta and props.
+        for id in meta_dict.keys():
+            meta = meta_dict[id]
+            props = props_dict[id]
+
+        # Check that the [X|Y]min values match.
+        if float(meta['Identifier.Xmin']) != float(props['Identifier.Xmin']):
+            raise ValueError(f"Xmin values do not match for ObjectCode {id} in {filename}.")
+        if float(meta['Identifier.Ymin']) != float(props['Identifier.Ymin']):
+            raise ValueError(f"Ymin values do not match for ObjectCode {id} in {filename}.")
+
+        # Check the the [X|Y]max values are off-by-one.
+        if float(meta['Identifier.Xmax']) != float(props['Identifier.Xmax']) - 1:
+            raise ValueError(f"Xmax values do not match for ObjectCode {id} in {filename}.")
+        if float(meta['Identifier.Ymax']) != float(props['Identifier.Ymax']) - 1:
+            raise ValueError(f"Ymax values do not match for ObjectCode {id} in {filename}.")
