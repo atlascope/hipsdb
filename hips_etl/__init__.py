7651c3c30473fe1dcb993507f67f8c379b42bfbe
Roni Choudhury <roni.choudhury@kitware.com>
2025-04-04 12:27:20 -0400

    Move helper functions into new utils submodule

--- hips_etl/__init__.py
+++ hips_etl/__init__.py
@@ -1,4 +1,3 @@
-import csv
 import importlib.resources
 import json
 import logging
@@ -6,9 +5,15 @@ import math
 from pathlib import Path
 import re
 import sys
-from typing import Literal
 
 import blessings
+from hips_etl.utils import (
+    dir_exists,
+    check_same_filenames,
+    read_csv,
+    fields_match,
+    get_object_mapping,
+)
 
 
 def initialize_logging() -> tuple[logging.Logger, logging.Formatter]:
@@ -78,47 +83,6 @@ props_only_fields = get_json_fields("props_only.json")
 
 with open(importlib.resources.files(__package__) / "fields" / "types.json") as f:
     types = json.load(f)
-
-
-def dir_exists(directory: Path) -> bool:
-    """Check if a directory exists and is a directory."""
-    return directory.exists() and directory.is_dir()
-
-
-def check_same_filenames(dir1: Path, dir2: Path):
-    """
-    Check if two directories contain the same set of filenames.
-    Returns the set of filenames if they match, otherwise returns None.
-    """
-    files1 = set(dir1.iterdir())
-    files2 = set(dir2.iterdir())
-    if {x.name for x in files1} != {x.name for x in files2}:
-        return None
-    return {x.name for x in files1}
-
-
-def read_csv(csv_file: Path) -> tuple[list[dict], list[str]]:
-    """Read a CSV file and return its contents as a list of dictionaries."""
-    with open(csv_file) as f:
-        reader = csv.DictReader(f)
-        fields = set(reader.fieldnames)
-        return (list(reader), fields)
-
-
-def fields_match(fields: set, expected_fields: set) -> bool:
-    missing_fields = expected_fields - fields
-    extra_fields = fields - expected_fields
-    if missing_fields or extra_fields:
-        logger.error(f"Fields mismatch: missing {missing_fields}, extra {extra_fields}")
-        return False
-    return True
-
-
-def get_object_mapping(rows: list[dict]) -> dict[int, dict] | None:
-    """Construct a mapping from ObjectCode to row for a list of dictionaries."""
-    mapping = {int(float(row["Identifier.ObjectCode"])): row for row in rows}
-
-    return mapping if len(mapping) == len(rows) else None
 
 
 def convert_intfloat(value: str) -> int | None:
---
+++ hips_etl/utils/__init__.py
@@ -0,0 +1,15 @@
+from .helpers import (
+    dir_exists,
+    check_same_filenames,
+    read_csv,
+    fields_match,
+    get_object_mapping,
+)
+
+__all__ = [
+    "dir_exists",
+    "check_same_filenames",
+    "read_csv",
+    "fields_match",
+    "get_object_mapping",
+]
