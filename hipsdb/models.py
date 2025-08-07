e920da27f699ea10a3fa05ca020602415c9eee0b
Roni Choudhury <roni.choudhury@kitware.com>
2025-08-07 13:18:54 -0400

    Allow duplicate image names
    Images will be distinguished by their ID, creation time, and (of course)
    data content.

--- hipsdb/models.py
+++ hipsdb/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Image(models.Model):
-    name: str = models.CharField(max_length=255, unique=True)
+    name: str = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
